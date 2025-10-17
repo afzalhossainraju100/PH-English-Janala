5e2727504813ff44cf25c1a61407b6c8e6c6d227
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2025-10-18 01:49:49 +0600

    Remove Unnecessary file now the file is ready to deliver to clint

--- test.js
+++
@@ -1,6 +0,0 @@
-const synonyms = ["enthusiastic", "eager", "keen", "ardent", "fervent"];
-
-const createElement =(arr)=>{
-    const htmlElements = arr.map(item => `<span class="btn">${item}</span>`).join('');
-    console.log(htmlElements);
-}
